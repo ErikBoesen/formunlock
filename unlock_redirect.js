084e9366015e872b15f70de68b7348d174b73bd3
ErikBoesen <me@erikboesen.com>
2018-09-24 18:26:12 -0400

    Complete redirect script

--- unlock_redirect.js
+++ unlock_redirect.js
@@ -1,2 +1,7 @@
 console.log('Switching to user 1!');
-
+var loc = window.location.href,
+    split_loc = loc.split('/');
+split_loc.splice(3, 0, 'u/1');
+split_loc.pop();
+split_loc.push('viewform');
+window.location.href = split_loc.join('/');
